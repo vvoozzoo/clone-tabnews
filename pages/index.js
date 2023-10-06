6d3859cf54971c0d00379a98776d3dee84ce20f9
vvoozzoo <146143656+vvoozzoo@users.noreply.github.com>
2023-10-06 00:22:18 +0000

    Frase para quem amo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home (){
-    return <h1>Estudando o building para ajudar o amigo. 2</h1>
+    return <h1>Tatinha, te amo! Obrigado pelo apoio, 'bejameuslabios' 😍</h1>
 }
 
 export default Home;
