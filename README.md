317850a3bb3615c9cf79f463ddb38acb73a8e838
vvoozzoo <146143656+vvoozzoo@users.noreply.github.com>
2023-10-11 01:33:26 +0000

    adiciona os scripts `lint:check`e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Repositório para fins didáticos em acompanhamento ao curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
-function Home (){
-    return <h1>Tatinha, te amo! Obrigado pelo apoio, 'bejameuslabios' 😍</h1>
+function Home() {
+  return <h1>Tatinha, te amo! Obrigado pelo apoio, 'bejameuslabios' 😍</h1>;
+}
+
+function Teste() {
+  console.log("Lol testei");
 }
 
 export default Home;
-
